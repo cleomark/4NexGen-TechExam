97be36bde821ee388eadb66cc55ab575d30fefce
cleomark <cleocalitisin@gmail.com>
2024-08-15 15:25:53 +0800

    version debug

--- nodejs-microservice-template/category-management/src/app/category.service.ts
+++ nodejs-microservice-template/category-management/src/app/category.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  private baseUrl = 'http://localhost:3000'; // Update with your Fastify server URL
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/categories`);
+  }
+
+  createCategory(category: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/category`, category);
+  }
+
+  updateCategory(id: number, category: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/category/${id}`, category);
+  }
+
+  deleteCategory(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/category/${id}`);
+  }
 }
