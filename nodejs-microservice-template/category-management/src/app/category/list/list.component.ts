d4b2afed0904135c94428211f945b0d1159112cc
cleomark <cleocalitisin@gmail.com>
2024-08-15 15:41:20 +0800

    set up list component

--- nodejs-microservice-template/category-management/src/app/category/list/list.component.ts
+++ nodejs-microservice-template/category-management/src/app/category/list/list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../category.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
-export class ListComponent {}
+export class ListComponent {
+  categories: any[] = [];
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe({
+      next: (data) => (this.categories = data),
+      error: (error) => console.error('Error fetching categories:', error),
+    });
+  }
+
+  deleteCategory(id: number) {
+    this.categoryService.deleteCategory(id).subscribe({
+      next: () =>
+        (this.categories = this.categories.filter(
+          (category) => category.id !== id
+        )),
+      error: (error) => console.error('Error deleting category:', error),
+    });
+  }
+}
