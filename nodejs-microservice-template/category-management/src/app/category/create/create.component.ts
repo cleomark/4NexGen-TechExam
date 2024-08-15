0d553dcb242baee3c08e90e06f5a0d2cd862b510
cleomark <cleocalitisin@gmail.com>
2024-08-15 15:39:05 +0800

    set up create component

--- nodejs-microservice-template/category-management/src/app/category/create/create.component.ts
+++ nodejs-microservice-template/category-management/src/app/category/create/create.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { CategoryService } from '../../category.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
 })
 export class CreateComponent {
+  categoryName: string = '';
 
+  constructor(private categoryService: CategoryService) {}
+
+  createCategory() {
+    this.categoryService
+      .createCategory({ category_name: this.categoryName })
+      .subscribe({
+        next: (data) => console.log('Category created:', data),
+        error: (error) => console.error('Error creating category:', error),
+      });
+  }
 }
